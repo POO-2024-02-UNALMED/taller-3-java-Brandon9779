ea1f774ca2d9ed6066fe89baa29876a38ad0ddeb
Brandon9779 <elbrandon9779@gmail.com>
2024-11-16 16:04:05 -0500

    Update Control.java

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,54 @@
+package taller3.televisores;
+
+public class Control {
+    public TV tv;
+    
+
+    public void setTv(TV tv){
+        return;
+    }
+
+    public TV getTv(){
+        return tv;
+    }
+
+    public void enlazar(TV televisor){
+        televisor = tv;
+    }
+
+    public void setCanal(int canal){
+        return;
+    }
+
+
+    public void setvolumen(int volumen){
+        return;
+    }
+
+    public void turnOff(){
+        turnOff();
+    }
+
+    public void turnOn(){
+        turnOn();
+    }
+
+    public void canalUp(){
+        canalUp();
+    }
+
+    public void canalDown(){
+        canalDown();
+
+    }
+
+    public void volumenUp(){
+        volumenUp();
+
+    }
+
+    public void volumenDown(){
+        volumenDown();
+    }
+    
+}
