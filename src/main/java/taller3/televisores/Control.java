90510bc164c399d10ca7e1c4b4269f6814fa153b
Brandon9779 <elbrandon9779@gmail.com>
2024-11-16 20:50:35 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -2,7 +2,7 @@ package taller3.televisores;
 
 public class Control {
     public TV tv;
-
+    
     public Control(){
         this.tv = null;
     }
@@ -21,51 +21,35 @@ public class Control {
         System.out.println("El control remoto se ha enlazado con el televisor.");
     }
 
-    public void canalUp(){
-        if (tv != null && tv.getEstado()) {
-            tv.canalUp();
-        } else {
-            System.out.println("El televisor no está encendido o no está enlazado.");
-        }
+    public void setCanal(int canal){
+        tv.setCanal(canal);
     }
 
-    public void canalDown(){
-        if (tv != null && tv.getEstado()) {
-            tv.canalDown();
-        } else {
-            System.out.println("El televisor no está encendido o no está enlazado.");
-        }
-    }
-
-    public void volumenUp(){
-        if (tv != null && tv.getEstado()) {
-            tv.volumenUp();
-        } else {
-            System.out.println("El televisor no está encendido o no está enlazado.");
-        }
-    }
-
-    public void volumenDown(){
-        if (tv != null && tv.getEstado()) {
-            tv.volumenDown();
-        } else {
-            System.out.println("El televisor no está encendido o no está enlazado.");
-        }
+    public void setVolumen(int volumen){
+        tv.setVolumen(volumen);
     }
 
     public void turnOff(){
-        if (tv != null) {
-            tv.turnOff();
-        } else {
-            System.out.println("El televisor no está enlazado.");
-        }
+        tv.turnOff();
     }
 
     public void turnOn(){
-        if (tv != null) {
-            tv.turnOn();
-        } else {
-            System.out.println("El televisor no está enlazado.");
-        }
+        tv.turnOn();
+    }
+
+    public void canalUp(){
+        tv.canalUp();
+    }
+
+    public void canalDown(){
+        tv.canalDown();
+    }
+
+    public void volumenUp(){
+        tv.volumenUp();
+    }
+
+    public void volumenDown(){
+        tv.volumenDown();
     }
 }
